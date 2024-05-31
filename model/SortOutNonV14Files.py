1fff579e16e695bef7ecb031e81e3b67eba5b3f5
hoelzflo <florian.hoelz@student.uni-augsburg.de>
2024-05-31 05:03:34 +0200

    Fix to prevent issues when sorting out files

--- model/SortOutNonV14Files.py
+++ model/SortOutNonV14Files.py
@@ -20,12 +20,17 @@ not_v14_dir = os.path.join(osu_files_dir, "not_v14")
 if not os.path.exists(not_v14_dir):
     os.makedirs(not_v14_dir)
 
-for i, osu_file_path in enumerate(tqdm(osu_file_paths)):
-    osu_file_path = os.path.join(osu_files_dir, osu_file_path)
+print(osu_file_paths[:10])
+
+for i, osu_file_name in enumerate(tqdm(osu_file_paths)):
+    osu_file_path = os.path.join(osu_files_dir, osu_file_name)
     if not os.path.isfile(osu_file_path):
         continue
     with open(osu_file_path, 'r', encoding='UTF-8') as osu_file:
-        first_line = osu_file.readline().strip()
+        try:
+            first_line = osu_file.readline().strip()
+        except Exception as e:
+            print(f'Error for {osu_file_name}: {e}')
         if first_line == config['required_file_version']:
             v14_counter += 1
         else:
@@ -33,7 +38,7 @@ for i, osu_file_path in enumerate(tqdm(osu_file_paths)):
             # Close the file before moving
             osu_file.close()
             # Move the file to the "not_v14" subfolder
-            shutil.move(os.path.join(osu_files_dir, osu_file_path), 
-                        os.path.join(not_v14_dir, osu_file_path))
+            shutil.move(os.path.join(osu_files_dir, osu_file_name), 
+                        os.path.join(not_v14_dir, osu_file_name))
 
 print(f'v14: {v14_counter}, not v14: {non_v14_counter}')
